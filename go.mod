ae5c7715231132b30a505fe20715366895edb78e
Gorgolevskiy Pavel <p.gorgolevskiy@potok.digital>
2020-03-06 01:06:06 +0300

    init commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/mechos3d/telegram_bot_simple
+
+go 1.12
+
+require (
+	github.com/Syfaro/telegram-bot-api v4.6.4+incompatible
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Syfaro/telegram-bot-api"
+	"log"
+	"os"
+)
+
+func main() {
+	fmt.Println("Boooooo")
+	token := os.Getenv("TELEGRAM_BOT_TOKEN")
+
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		fmt.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
+}
